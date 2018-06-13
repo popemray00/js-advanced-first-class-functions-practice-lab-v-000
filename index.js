f1ba10e268491362c5e4565d67755a50fcb05a33
Michael Pope <popemray1986@gmail.com>
2018-06-13 04:38:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const callback = function (el, i, arr) {
   console.log('The current array is', arr);
   console.log('----------------');
 };
+
+const myForEach = function (arr, cb) {
+  for (const el of arr) {
+    cb(el, arr.indexOf(el), arr);
+  }
+};
