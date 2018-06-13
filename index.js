3241491558940ddbc34204e74049a6c38bdb8973
Michael Pope <popemray1986@gmail.com>
2018-06-13 04:37:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file!
-const callback = function (el, i, arr) {
+const logDriverNames = function (el, i, arr) {
   console.log('The current element is', el);
   console.log('The index of the current element is', i);
   console.log('The current array is', arr);
